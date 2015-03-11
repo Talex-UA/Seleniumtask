dd0336d318434825779fb30ddb263b997f385bfa
Talex-UA <talexua91@gmail.com>
2015-03-11 17:16:44 +0200

    Another try to fix test with ping. Element initialization after scroll down.

--- src/main/java/JenkinsDashboard/Pages/FreestylePropertiesPage.java
+++ src/main/java/JenkinsDashboard/Pages/FreestylePropertiesPage.java
@@ -173,8 +173,9 @@ public class FreestylePropertiesPage extends SecuredPage<FreestylePropertiesPage
     public FreestylePropertiesPage addWindowsBatchCommand() {
         JavascriptExecutor jse = (JavascriptExecutor)wd;
         jse.executeScript("window.scrollTo(0,Math.max(document.documentElement.scrollHeight,document.body.scrollHeight,document.documentElement.clientHeight));");
-        addBuildStepButton.click();
+//        addBuildStepButton.click();
 
+        wd.findElement(By.cssSelector("[suffix=builder]")).click();
         for (WebElement currentElement:getDropDowList()){
             if (currentElement.getText().contains("Windows")){
                 currentElement.click();
